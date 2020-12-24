4ce5316a40bc4fa6afc4b111bf4d02b689cfed1b
Marco Lopez <inglduarte@gmail.com>
2020-12-23 20:12:06 -0800

    update packages

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const Post = require("./models/Post");
 
 // Connect to MLab Database
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology:true })
   .then(() => console.log("DB connected"))
   .catch(err => {
     console.error('Error')
